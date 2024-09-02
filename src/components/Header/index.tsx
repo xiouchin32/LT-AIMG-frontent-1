19708964de661ee4583e07047e3e89204288fbdf
馬琇衾 <>
2024-09-02 14:16:58 +0800

    feat: add logo click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,29 +1,32 @@
 'use client'
 
-import { useState } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import Image from 'next/image'
+import Link from 'next/link'
+import { useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai'
 
 export default function HeaderIndex() {
-  const [isOpen, setIsOpen] = useState(false);
-  
+  const [isOpen, setIsOpen] = useState(false)
+
   const menu = [
     { text: '關於計畫' },
     { text: '導入指引' },
     { text: '輔導團' },
     { text: '技術支援' },
     { text: '人才培育' },
-    { text: '輔助資源' },
-  ];
+    { text: '輔助資源' }
+  ]
 
   const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+    setIsOpen(!isOpen)
+  }
 
   return (
     <>
       <header className='w-[100%] h-[60px] lg:w-full fixed top-0 left-1/2 -translate-x-1/2 py-2 px-6 bg-white flex items-center justify-between z-[1102] lg:z-[1100]'>
-        <div className='text-xl font-bold'>Logo</div>
-        
+        <Link href="/">
+          <Image src={'/logo.svg'} width={150} height={150} alt='Logo' />
+        </Link>
         {/* Desktop Menu */}
         <nav className='hidden lg:flex space-x-6'>
           {menu.map((item, index) => (
@@ -65,5 +68,5 @@ export default function HeaderIndex() {
         </div>
       </div>
     </>
-  );
+  )
 }
